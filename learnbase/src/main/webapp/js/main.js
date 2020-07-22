b55b86cff58de7c8410cbca8f72fcac058021d3a
Daniel Adebi <adebiike@outlook.com>
2020-07-22 16:57:58 -0400

    deleted test ML code

--- learnbase/src/main/webapp/js/main.js
+++ learnbase/src/main/webapp/js/main.js
@@ -1,15 +1,1 @@
 "use strict";
-const model = tf.sequential();
-model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
-model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
-// Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-model.fit(xs, ys, {
-    epochs: 100,
-    callbacks: {
-        onEpochEnd: (epoch, log) => console.log(`Epoch ${epoch}: loss = ${log.loss}`)
-    }
-});
-console.log(xs);
-console.log(ys);
