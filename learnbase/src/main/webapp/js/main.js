3e77db4ae19d33c0fe6cd53c078101a188e725a3
Daniel Adebi <adebiike@outlook.com>
2020-07-22 15:53:10 -0400

    More changes

--- learnbase/src/main/webapp/js/main.js
+++ learnbase/src/main/webapp/js/main.js
@@ -1,4 +1,4 @@
-import * as tf from "@tensorflow/tfjs";
+"use strict";
 const model = tf.sequential();
 model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
 model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
--- learnbase/src/main/webapp/ts/main.ts
+++ learnbase/src/main/webapp/ts/main.ts
@@ -1,5 +1,3 @@
-import * as tf from "@tensorflow/tfjs";
-
 const model = tf.sequential();
 model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
