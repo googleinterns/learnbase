c4fe18fe75cda1c4c7a9da68efd1079aad5053d4
Federickg23 <Federickg23@gmail.com>
2020-07-23 15:04:09 -0400

    Link new js file and fix error in onload

--- learnbase/src/main/webapp/ts/main.ts
+++ learnbase/src/main/webapp/ts/main.ts
@@ -4,7 +4,11 @@ console.log("tester");
 window.onload = function userLogin() : void {
   fetch('/userlogin').then(response => response.text()).then((pageContent) => {
     const loginSection = document.getElementById('user-page-content') as HTMLDivElement;
-    loginSection.innerHTML = pageContent;
+    try{
+      loginSection.innerHTML = pageContent;
+    } catch (error){
+      console.log(error); 
+    }
   });
 } 
 
