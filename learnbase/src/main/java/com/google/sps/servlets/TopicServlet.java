23023c911be147d7bb9b7cb461717712bc8f7501
Federickg23 <Federickg23@gmail.com>
2020-07-23 15:00:08 -0400

    Semicolon

--- learnbase/src/main/java/com/google/sps/servlets/TopicServlet.java
+++ learnbase/src/main/java/com/google/sps/servlets/TopicServlet.java
@@ -19,7 +19,7 @@ public class TopicServlet extends HttpServlet{
         UserService userService = UserServiceFactory.getUserService(); 
         System.out.println("In Get request");
         System.out.println(request.getUserPrincipal());
-        response.getWriter().println(request.getUserPrincipal().getName())
+        response.getWriter().println(request.getUserPrincipal().getName());
     }
 
     @Override
