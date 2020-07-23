19b9e66e274e549cbf0fe521d236b801545bb6e7
Daniel Adebi <adebiike@outlook.com>
2020-07-23 08:31:55 -0400

    Fixed the error where userLogin function isn't recognized

--- learnbase/src/main/webapp/ts/main.ts
+++ learnbase/src/main/webapp/ts/main.ts
@@ -1,8 +1,8 @@
 console.log("tester");
 
-function userLogin() {
-    fetch('/userlogin').then(response => response.json()).then((pageContent) => {
-        const loginSection = document.getElementById('user-page-content');
-        loginSection.appendChild(pageContent);
-    });
+window.onload = function userLogin() {
+  fetch('/userlogin').then(response => response.json()).then((pageContent) : void => {
+    const loginSection = document.getElementById('user-page-content') as HTMLDivElement;
+    loginSection.appendChild(pageContent);
+  });
 }
