50e09c9b3ee4aa241cbd6c9db6156ee5384b63ad
Federickg23 <Federickg23@gmail.com>
2020-07-24 11:02:19 -0400

    Compile typescript

--- learnbase/src/main/webapp/js/search.js
+++ learnbase/src/main/webapp/js/search.js
@@ -4,9 +4,14 @@
 window.onload = function getTopics() {
     fetch('/topics').then(response => response.json()).then((response) => {
         console.log(response);
-        var topics = JSON.parse(response);
-        console.log(topics);
-        var table = document.getElementById("subjectTable");
-        console.log("get topics");
+        topicManager(response);
     });
 };
+function topicManager(topics) {
+    var table = document.getElementById('subjectTable');
+    topics.array.forEach(topic => {
+        var newRow = table.insertRow();
+        var cell = newRow.insertCell();
+        cell.innerHTML = topic;
+    });
+}
