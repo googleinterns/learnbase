49270b479abe43df83ee391bd4be32ffd47510a5
Daniel Adebi <adebiike@outlook.com>
2020-07-26 18:21:27 -0400

    edited a lil java

--- learnbase/src/main/java/com/google/sps/servlets/PythonServlet.java
+++ learnbase/src/main/java/com/google/sps/servlets/PythonServlet.java
@@ -39,7 +39,6 @@ public class PythonServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-       
     Gson gson = new Gson();
     wordInfo = gson.fromJson(new FileReader("py/json/word_cache.json"), 
         new TypeToken<HashMap<String, ArrayList<String>>>() {}.getType());
