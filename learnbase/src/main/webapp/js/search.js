a9ba4512be1283b209f7f04c4fb932576ed84ab7
Daniel Adebi <adebiike@outlook.com>
2020-07-28 15:20:54 -0400

    Recommendation system now has implemented bias towards subjects more recently selected

--- learnbase/src/main/webapp/js/search.js
+++ learnbase/src/main/webapp/js/search.js
@@ -18,9 +18,62 @@ window.onload = function getTopics() {
     fetch('/topics').then(response => response.json()).then((response) => {
         console.log(response);
         topicManager(response);
-        getSimilarTopics(response[response.length - 1]);
+        getRecommendedTopics(response);
     });
 };
+function getRecommendedTopics(response) {
+    return __awaiter(this, void 0, void 0, function* () {
+        var topicInfoList = [];
+        var recsPerTopic = [];
+        var recommendations = [];
+        for (let i = response.length - 1; i >= 0; i--) {
+            let topic = response[i];
+            let similarTopics;
+            let topicInfo = yield getSimilarTopics(topic).then((results) => {
+                similarTopics = results;
+                let topicTuple = [topic, similarTopics];
+                return topicTuple;
+            });
+            topicInfoList.push(topicInfo);
+            recsPerTopic.push(0);
+        }
+        // First topic has 4 automatic recommendations from most recent choice.
+        // Other 6 are drawn from random distribution of all topics.
+        for (let i = 0; i < 6; i++) {
+            let rand = Math.random() * 10000;
+            let j = 1;
+            if (rand < 10000 / Math.pow(2, topicInfoList.length - 1)) {
+                recsPerTopic[0] += 1;
+            }
+            while (j < topicInfoList.length) {
+                if (rand > 10000 / Math.pow(2, j)) {
+                    recsPerTopic[j] += 1;
+                    break;
+                }
+                j++;
+            }
+            console.log(rand);
+        }
+        let currIndex = 0;
+        for (let i = 0; i < 10; i++) {
+            if (i < 4) {
+                recommendations.push(topicInfoList[0][1][i]);
+            }
+            else {
+                if (recsPerTopic[currIndex] === 0) {
+                    currIndex += 1;
+                }
+                else {
+                    recsPerTopic[currIndex] -= 1;
+                }
+                let rand = (currIndex === 0) ? Math.floor(Math.random() * 6) + 4 : Math.floor(Math.random() * 10);
+                let nextTopic = topicInfoList[currIndex][1][rand];
+                recommendations.push(nextTopic);
+            }
+        }
+        console.log("Recommendations: " + recommendations);
+    });
+}
 function topicManager(topics) {
     var table = document.getElementById('subjectTable');
     topics.forEach((topic) => {
@@ -48,7 +101,6 @@ function getSimilarTopics(topic) {
         console.log(`topic: ${topic}`);
         const response = yield fetch(`/recommend-topics?topic=${topic}`);
         const similarTopics = yield response.json();
-        console.log(similarTopics);
         return similarTopics;
     });
 }
--- learnbase/src/main/webapp/ts/search.ts
+++ learnbase/src/main/webapp/ts/search.ts
@@ -1,5 +1,7 @@
 "use strict";
 
+type TopicInfo = [string, string[]];
+
 document.getElementById("timeChange").addEventListener("click", timeChange);
 //document.getElementById("body").addEventListener("load", getTopics);
 
@@ -8,13 +10,77 @@ function timeChange(){
   document.getElementById("currentTime").style.direction = "none";
 }
 
-
 window.onload = function getTopics() : void {
   fetch('/topics').then(response => response.json()).then((response) =>{
     console.log(response);
     topicManager(response);
-    getSimilarTopics(response[response.length - 1]);
+
+    getRecommendedTopics(response);
   })  
+}
+
+async function getRecommendedTopics(response: string)  {
+  var topicInfoList : TopicInfo[] = [];
+  var recsPerTopic : number[] = [];
+  
+  var recommendations : string[] = [];
+
+  for (let i = response.length-1; i >= 0; i--) {
+    let topic : string = response[i];
+    let similarTopics : string[];
+
+    let topicInfo : TopicInfo = await getSimilarTopics(topic).then((results : string[]) => {
+      similarTopics = results;
+      let topicTuple : TopicInfo = [topic, similarTopics];
+
+      return topicTuple;
+    });
+
+    topicInfoList.push(topicInfo);
+    recsPerTopic.push(0);  
+  }
+
+  // First topic has 4 automatic recommendations from most recent choice.
+  // Other 6 are drawn from random distribution of all topics.
+  for (let i = 0; i < 6; i++) {
+    let rand : number = Math.random()*10000;
+    let j = 1;
+    
+    if (rand < 10000/Math.pow(2, topicInfoList.length-1)) {
+      recsPerTopic[0] += 1;
+    }
+    while (j < topicInfoList.length) {
+      if (rand > 10000/Math.pow(2, j)) {
+        recsPerTopic[j] += 1;
+        break;
+      }
+      j++;
+    }
+
+    console.log(rand);
+
+  }
+  
+  let currIndex : number = 0;
+  for (let i = 0; i < 10; i++) {
+    if (i < 4) {
+      recommendations.push(topicInfoList[0][1][i]);
+    } else {
+      
+      if (recsPerTopic[currIndex] === 0) {
+        currIndex += 1;
+      } else {
+        recsPerTopic[currIndex] -= 1
+      }
+      
+      let rand : number = (currIndex === 0) ? Math.floor(Math.random()* 6) + 4 : Math.floor(Math.random() * 10);
+      let nextTopic : string = topicInfoList[currIndex][1][rand];
+      
+      recommendations.push(nextTopic);
+    }
+  }
+
+  console.log("Recommendations: " + recommendations);
 }
 
 function topicManager(topics: string[]) : void {
@@ -43,12 +109,11 @@ function deleteTopic(topic: string) : void {
 }
 
 // TODO: Change functionality so that it loads on submit
-async function getSimilarTopics(topic: string) : Promise<void> {
+async function getSimilarTopics(topic: string) : Promise<string[]> {
   console.log(`topic: ${topic}`);
 
   const response = await fetch(`/recommend-topics?topic=${topic}`);
   const similarTopics = await response.json();
 
-  console.log(similarTopics);
   return similarTopics;
 }
