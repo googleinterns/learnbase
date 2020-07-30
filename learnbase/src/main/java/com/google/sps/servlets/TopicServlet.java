2a206f034eafe0dde791401b54ea38bbb268144e
Federickg23 <Federickg23@gmail.com>
2020-07-30 11:06:37 -0400

    Fixing errors caused by newline

--- learnbase/src/main/java/com/google/sps/servlets/TopicServlet.java
+++ learnbase/src/main/java/com/google/sps/servlets/TopicServlet.java
@@ -102,8 +102,7 @@ public class TopicServlet extends HttpServlet{
         .put("Name", "Federick")))
         .put(Emailv31.Message.SUBJECT, "Greetings from Mailjet.")
         .put(Emailv31.Message.TEXTPART, "My first Mailjet email")
-        .put(Emailv31.Message.HTMLPART, "<h3>Dear passenger 1, welcome to 
-        <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!")
+        .put(Emailv31.Message.HTMLPART, "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!")
         .put(Emailv31.Message.CUSTOMID, "AppGettingStartedTest")));
         response = client.post(request);
         System.out.println(response.getStatus());
