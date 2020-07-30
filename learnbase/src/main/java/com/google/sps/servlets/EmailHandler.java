0d9eb6295ab07a8bdbc6c977b57f2c946122ee6f
Federickg23 <Federickg23@gmail.com>
2020-07-30 11:14:45 -0400

    Move email code to other class and call within topic servlet

---
+++ learnbase/src/main/java/com/google/sps/servlets/EmailHandler.java
@@ -0,0 +1,43 @@
+package com.google.sps.servlets;
+
+
+import com.mailjet.client.errors.MailjetException;
+import com.mailjet.client.errors.MailjetSocketTimeoutException;
+import com.mailjet.client.MailjetClient;
+import com.mailjet.client.MailjetRequest;
+import com.mailjet.client.MailjetResponse;
+import com.mailjet.client.ClientOptions;
+import com.mailjet.client.resource.Emailv31;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class EmailHandler{
+
+  public EmailHandler(){
+    
+  }
+
+  public void sendMail() throws MailjetException, MailjetSocketTimeoutException {
+    MailjetClient client;
+    MailjetRequest request;
+    MailjetResponse response;
+    client = new MailjetClient(System.getenv("2065063d2f679c68571c386cf8d13767"), System.getenv("2921119afe3fec7e64e36d4677fc4a75"), new ClientOptions("v3.1"));
+    request = new MailjetRequest(Emailv31.resource)
+    .property(Emailv31.MESSAGES, new JSONArray()
+    .put(new JSONObject()
+    .put(Emailv31.Message.FROM, new JSONObject()
+    .put("Email", "thefed@google.com")
+    .put("Name", "Federick"))
+    .put(Emailv31.Message.TO, new JSONArray()
+    .put(new JSONObject()
+    .put("Email", "thefed@google.com")
+    .put("Name", "Federick")))
+    .put(Emailv31.Message.SUBJECT, "Greetings from Mailjet.")
+    .put(Emailv31.Message.TEXTPART, "My first Mailjet email")
+    .put(Emailv31.Message.HTMLPART, "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!")
+    .put(Emailv31.Message.CUSTOMID, "AppGettingStartedTest")));
+    response = client.post(request);
+    System.out.println(response.getStatus());
+    System.out.println(response.getData());
+  }
+}
--- learnbase/src/main/java/com/google/sps/servlets/TopicServlet.java
+++ learnbase/src/main/java/com/google/sps/servlets/TopicServlet.java
@@ -19,15 +19,7 @@ import java.net.URLEncoder;
 import java.net.URLDecoder;
 import java.io.*; 
 import java.util.*; 
-import com.mailjet.client.errors.MailjetException;
-import com.mailjet.client.errors.MailjetSocketTimeoutException;
-import com.mailjet.client.MailjetClient;
-import com.mailjet.client.MailjetRequest;
-import com.mailjet.client.MailjetResponse;
-import com.mailjet.client.ClientOptions;
-import com.mailjet.client.resource.Emailv31;
-import org.json.JSONArray;
-import org.json.JSONObject;
+
 
 @WebServlet("/topics")
 public class TopicServlet extends HttpServlet{
@@ -83,33 +75,8 @@ public class TopicServlet extends HttpServlet{
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        ///////////////////////////////////////////////////////////////////////////////////////////////
-        //Email stuff
-        MailjetClient client;
-        MailjetRequest request;
-        MailjetResponse response;
-        client = new MailjetClient(System.getenv("2065063d2f679c68571c386cf8d13767"), 
-            System.getenv("2921119afe3fec7e64e36d4677fc4a75"), new ClientOptions("v3.1"));
-        request = new MailjetRequest(Emailv31.resource)
-        .property(Emailv31.MESSAGES, new JSONArray()
-        .put(new JSONObject()
-        .put(Emailv31.Message.FROM, new JSONObject()
-        .put("Email", "thefed@google.com")
-        .put("Name", "Federick"))
-        .put(Emailv31.Message.TO, new JSONArray()
-        .put(new JSONObject()
-        .put("Email", "thefed@google.com")
-        .put("Name", "Federick")))
-        .put(Emailv31.Message.SUBJECT, "Greetings from Mailjet.")
-        .put(Emailv31.Message.TEXTPART, "My first Mailjet email")
-        .put(Emailv31.Message.HTMLPART, "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!")
-        .put(Emailv31.Message.CUSTOMID, "AppGettingStartedTest")));
-        response = client.post(request);
-        System.out.println(response.getStatus());
-        System.out.println(response.getData());
-
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////
+        EmailHandler handler = new EmailHandler();
+        handler.sendMail();
         UserService userService = UserServiceFactory.getUserService(); 
         User user = userService.getCurrentUser();
         String userId = user.getUserId(); 
@@ -126,30 +93,30 @@ public class TopicServlet extends HttpServlet{
         String currentUrl = (String) entity.getProperty("currentUrl");
         String topic = request.getParameter("topic");
         String topics = (String) entity.getProperty("topics"); 
-	ArrayList<String> urls = (ArrayList<String>) entity.getProperty("urls");
-        
-        if(currentUrl == null) { 
-          currentUrl = "0";
-	}
-        if (topics.equals("")){
-            entity.setProperty("topics", topic);
-        }  else {
-            topics += ",";
-            topics += topic;
-            entity.setProperty("topics", topics);
-        } 
-	if(urls == null) {
-          urls = new ArrayList<>();
+        ArrayList<String> urls = (ArrayList<String>) entity.getProperty("urls");
+            
+            if(currentUrl == null) { 
+            currentUrl = "0";
         }
-	String[] values = topics.split(",");
-        urls = getSearch(topic, urls);
-	System.out.println(urls);
-	getInfo(urls, currentUrl);
-	currentUrl =  Integer.toString(Integer.parseInt(currentUrl)+1); 
-	entity.setProperty("currentUrl", currentUrl);
-	entity.setProperty("urls", urls);
-	datastore.put(entity);
-        response.sendRedirect("/search.html");
+            if (topics.equals("")){
+                entity.setProperty("topics", topic);
+            }  else {
+                topics += ",";
+                topics += topic;
+                entity.setProperty("topics", topics);
+            } 
+        if(urls == null) {
+            urls = new ArrayList<>();
+            }
+        String[] values = topics.split(",");
+            urls = getSearch(topic, urls);
+        System.out.println(urls);
+        getInfo(urls, currentUrl);
+        currentUrl =  Integer.toString(Integer.parseInt(currentUrl)+1); 
+        entity.setProperty("currentUrl", currentUrl);
+        entity.setProperty("urls", urls);
+        datastore.put(entity);
+            response.sendRedirect("/search.html");
     }
 
   private ArrayList<String> getSearch(String topic, ArrayList<String> urls) throws IOException {
