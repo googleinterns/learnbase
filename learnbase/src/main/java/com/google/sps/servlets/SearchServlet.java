4adfc649eb0a3250a8edc68e4e11dcf9e0fc211b
Kerry O'Malley <komalley411@gmail.com>
2020-08-10 17:14:52 +0000

    Add links to webscrapping and loading circle on info page

--- learnbase/src/main/java/com/google/sps/servlets/SearchServlet.java
+++ learnbase/src/main/java/com/google/sps/servlets/SearchServlet.java
@@ -22,11 +22,11 @@ import java.util.*;
 
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
-
+  
+  //Gets the current days information for each topic 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json;");
-
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     UserService userService = UserServiceFactory.getUserService();
     User user = userService.getCurrentUser();
@@ -36,7 +36,10 @@ public class SearchServlet extends HttpServlet {
       .setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, userId));
     PreparedQuery results = datastore.prepare(query);
     Entity entity = results.asSingleEntity();
+   
     String topics = (String) entity.getProperty("topics");
+    
+    //If the user has not searched any topics yet, print that they have no topics
     if (topics.trim().equals("")) {
       Gson gson = new Gson();
       ArrayList<String> topicsInfo = new ArrayList<>();
@@ -44,44 +47,49 @@ public class SearchServlet extends HttpServlet {
       response.getWriter().println(gson.toJson(topicsInfo));
       return;
     }
-    System.out.println("topics:" + topics);
+
     ArrayList<String> topicsInfo = new ArrayList<>();
     String[] topicsArray = topics.split(",");
-    System.out.println("topics array:" + topicsArray);
+
+    //Goes through each topic and gets that days info, 
+    //then adds it to the arraylist that stores the info to print
     for (String topic : topicsArray) {
       String topicName = topic+"topic";
       String iteratorName = topic+"iterator";
-      System.out.println(iteratorName);
       String iterator = (String) entity.getProperty(iteratorName);
       ArrayList<String> urls = (ArrayList<String>) entity.getProperty(topicName);
       int iteratorNum = Integer.parseInt(iterator);
+      
+      //If there are no more urls for this topic, 
+      //Print that there are none and then go to next topic 
       if (iteratorNum >= urls.size()) {
 	topicsInfo.add(0,"No more info for this topic!");
 	topicsInfo.add(0, "<h1>"+topic+":</h1>");
 	continue;
       }
+
       String info = getInfo(urls, iterator);
       topicsInfo.add(0, info);
       topicsInfo.add(0, "<h1>"+topic+":</h1>");
+
+      //Increment iterator so that the next day they get new info 
       iterator = Integer.toString(Integer.parseInt(iterator)+1);
       entity.setProperty(iteratorName, iterator);
     }
-    System.out.println(topicsInfo);
     datastore.put(entity);
     Gson gson = new Gson();
     response.getWriter().println(gson.toJson(topicsInfo));
 
   }
   
+  //Gets the info off a page for a given url 
   private String getInfo(ArrayList<String> urls, String currentUrl) throws IOException {
     String info = "";
     int currentUrlNum = Integer.parseInt(currentUrl);
     String url = urls.get(currentUrlNum);
 
-    System.out.println(url);
-
     Document doc = Jsoup.connect(url).get();
-    Elements results = doc.select("p");
+    Elements results = doc.select("p, a");
 
     for(Element result : results) {
       info = info + result.toString();
