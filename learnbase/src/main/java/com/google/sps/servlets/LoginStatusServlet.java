664fbe697ada00bcaa4aef196ce6dd93c343ab6a
Federickg23 <Federickg23@gmail.com>
2020-08-18 14:26:58 -0400

    Prevent logged out users from accessing all but the homepage

---
+++ learnbase/src/main/java/com/google/sps/servlets/LoginStatusServlet.java
@@ -0,0 +1,32 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.datastore.*;
+import java.io.PrintWriter;
+import java.io.*; 
+import java.util.*; 
+
+
+@WebServlet("/status")
+public class LoginStatusServlet extends HttpServlet {
+  
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    response.setContentType("text/plain");
+
+    if (userService.isUserLoggedIn()){
+      response.getWriter().println("loggedIn");
+    } else{
+      response.getWriter().println("loggedOut");
+
+    }
+
+  }
