74d18f571495972111126737f2f2b3bf49e09ecb
Federickg23 <Federickg23@gmail.com>
2020-08-24 17:01:19 -0400

    Begin style fix

--- learnbase/src/main/java/com/google/sps/servlets/TopicServlet.java
+++ learnbase/src/main/java/com/google/sps/servlets/TopicServlet.java
@@ -26,129 +26,127 @@ import java.net.URLConnection;
 @WebServlet("/topics")
 public class TopicServlet extends HttpServlet{
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-        
-        response.setContentType("application/json;");
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{      
+    response.setContentType("application/json;");
 
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        UserService userService = UserServiceFactory.getUserService(); 
-        User user = userService.getCurrentUser();
-        String userId = user.getUserId(); 
-        Query query = 
-        new Query("UserInfo")
-        .setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, userId));
-        PreparedQuery results = datastore.prepare(query); 
-        Entity entity = results.asSingleEntity(); 
-        String topics = (String) entity.getProperty("topics"); 
-        System.out.println(topics);
-        if (topics.equals("")){
-            response.getWriter().println("{}");
-            return;
-        }
-
-        while (topics.length() > 0 && topics.substring(0,1).equals(",")){
-          //Clears commas from the beginning of the topics string to avoid blank topics
-            try{
-                if (topics.length() > 1){
-                    topics = topics.substring(1);
-                }
-                else{
-                    topics = "";
-                    entity.setProperty("topics", topics);
-                    datastore.put(entity); 
-                    response.getWriter().println("{}");
-                    return;
-                }
-            } catch (Exception e){ 
-                topics = "";
-                entity.setProperty("topics", topics);
-                datastore.put(entity); 
-
-                response.getWriter().println("{}");
-                return;
-            }
-
-        }
-        String [] listedTopics = topics.split(",");
-        for (int i = 0; i < listedTopics.length; i++){
-          listedTopics[i] = listedTopics[i].substring(0,1).toUpperCase() + listedTopics[i].substring(1);
-        }
-        System.out.println(Arrays.toString(listedTopics));
-        Gson gson = new Gson(); 
-        String returnTopics = gson.toJson(listedTopics);
-        response.getWriter().println(returnTopics);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    UserService userService = UserServiceFactory.getUserService(); 
+    User user = userService.getCurrentUser();
+    String userId = user.getUserId(); 
+    Query query = 
+    new Query("UserInfo")
+    .setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, userId));
+    PreparedQuery results = datastore.prepare(query); 
+    Entity entity = results.asSingleEntity(); 
+    String topics = (String) entity.getProperty("topics"); 
+    System.out.println(topics);
+    if (topics.equals("")){
+      response.getWriter().println("{}");
+      return;
     }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        UserService userService = UserServiceFactory.getUserService(); 
-        User user = userService.getCurrentUser();
-        String userId = user.getUserId(); 
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        Query query = new Query("UserInfo").setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, userId));
-        PreparedQuery results = datastore.prepare(query); 
-        Entity entity = results.asSingleEntity(); 
-        if (entity == null){
-            response.sendRedirect("/search.html");
+    while (topics.length() > 0 && topics.substring(0,1).equals(",")){
+    //Clears commas from the beginning of the topics string to avoid blank topics
+      try{
+        if (topics.length() > 1){
+          topics = topics.substring(1);
         }
-
-        String topic = request.getParameter("topic").trim().replaceAll(" +", " ").toLowerCase();
-	      String topicName = topic+"topic";
-        String iteratorName = topic+"iterator";
-	      String advanced = "advanced"+topic;
-
-        String topics = (String) entity.getProperty("topics"); 
-	      if (topics.contains(topic)) {
-          List<String> listOfTopics = new ArrayList<String>();
-          String str[] = topics.split(",");
-          for (int i = 0; i < str.length; i++) {
-            listOfTopics.add(str[i]);
-          }
-
-          if (!listOfTopics.contains(topic)) {
-      	    topics += ",";
-            topics += topic;
-          } else {
-            listOfTopics.remove(topic);
-            topics = "";
-            for (int i = 0; i < listOfTopics.size(); i++) {
-              topics += listOfTopics.get(i);
-              topics += ",";
-            }
-            topics += topic;
-          }
+        else{
+          topics = "";
           entity.setProperty("topics", topics);
-	        datastore.put(entity);
-          response.sendRedirect("/search.html");
+          datastore.put(entity); 
+          response.getWriter().println("{}");
           return;
-       }
-       ArrayList<String> urls = getSearch(topic);
-            
-       if (topics.equals("")){
-         entity.setProperty("topics", topic);
-       }  else {
-         topics += ",";
-         topics += topic;
-         entity.setProperty("topics", topics);
+        }
+      } catch (Exception e){ 
+        topics = "";
+        entity.setProperty("topics", topics);
+        datastore.put(entity); 
+
+        response.getWriter().println("{}");
+        return;
       }
-      datastore.put(entity); 
-      entity.setProperty(topicName, urls);
-      entity.setProperty(iteratorName, "0");
-      System.out.println(topicName + ": " + urls); 
-      entity.setProperty(advanced, false);
-      String[] values = topics.split(",");
-      datastore.put(entity);
-      String[] topicsArray = topics.split(",");
-      for (String thisTopic : topicsArray) { 
-        String thisTopicName = thisTopic+"topic";
-      	ArrayList<String> topicUrls = (ArrayList<String>) entity.getProperty(thisTopicName);
-        System.out.println(thisTopic+ ": ");
-        System.out.println(topicUrls);
+    }
+    String [] listedTopics = topics.split(",");
+    for (int i = 0; i < listedTopics.length; i++){
+      listedTopics[i] = listedTopics[i].substring(0,1).toUpperCase() + listedTopics[i].substring(1);
+    }
+    System.out.println(Arrays.toString(listedTopics));
+    Gson gson = new Gson(); 
+    String returnTopics = gson.toJson(listedTopics);
+    response.getWriter().println(returnTopics);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService(); 
+    User user = userService.getCurrentUser();
+    String userId = user.getUserId(); 
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query = new Query("UserInfo").setFilter(new Query.FilterPredicate("id", Query.FilterOperator.EQUAL, userId));
+    PreparedQuery results = datastore.prepare(query); 
+    Entity entity = results.asSingleEntity(); 
+    if (entity == null){
+      response.sendRedirect("/search.html");
+    }
+
+    String topic = request.getParameter("topic").trim().replaceAll(" +", " ").toLowerCase();
+    String topicName = topic+"topic";
+    String iteratorName = topic+"iterator";
+    String advanced = "advanced"+topic;
+
+    String topics = (String) entity.getProperty("topics"); 
+    if (topics.contains(topic)) {
+      List<String> listOfTopics = new ArrayList<String>();
+      String str[] = topics.split(",");
+      for (int i = 0; i < str.length; i++) {
+        listOfTopics.add(str[i]);
       }
 
+      if (!listOfTopics.contains(topic)) {
+        topics += ",";
+        topics += topic;
+      } else {
+        listOfTopics.remove(topic);
+        topics = "";
+        for (int i = 0; i < listOfTopics.size(); i++) {
+          topics += listOfTopics.get(i);
+          topics += ",";
+        }
+        topics += topic;
+      }
+      entity.setProperty("topics", topics);
+      datastore.put(entity);
       response.sendRedirect("/search.html");
+      return;
+    }
+    ArrayList<String> urls = getSearch(topic);
+        
+    if (topics.equals("")){
+      entity.setProperty("topics", topic);
+    }  else {
+      topics += ",";
+      topics += topic;
+      entity.setProperty("topics", topics);
+    }
+    datastore.put(entity); 
+    entity.setProperty(topicName, urls);
+    entity.setProperty(iteratorName, "0");
+    System.out.println(topicName + ": " + urls); 
+    entity.setProperty(advanced, false);
+    String[] values = topics.split(",");
+    datastore.put(entity);
+    String[] topicsArray = topics.split(",");
+    for (String thisTopic : topicsArray) { 
+      String thisTopicName = thisTopic+"topic";
+      ArrayList<String> topicUrls = (ArrayList<String>) entity.getProperty(thisTopicName);
+      System.out.println(thisTopic+ ": ");
+      System.out.println(topicUrls);
+    }
+
+    response.sendRedirect("/search.html");
   }
  
   //Given a topic, gets urls from google 
@@ -162,27 +160,27 @@ public class TopicServlet extends HttpServlet{
     Elements results = doc.select("a[href]:has(span)").select("a[href]:not(:has(div))");
 
     for (Element result : results) {
-        String linkHref = result.attr("href");
-        String linkText = result.text();
-        if (linkHref.contains("https")) {
-	  String url = linkHref.substring(7, linkHref.indexOf("&"));
-	  if("/www.google.com/search?num=20".equals(url)) {
-            continue;
-	  }
-	  URL obj = new URL(url);
-	  URLConnection conn = obj.openConnection();
-	  Map<String, List<String>> map = conn.getHeaderFields();
-	  boolean noIFrame = false;
-	  for(Map.Entry<String, List<String>> entry : map.entrySet()) {
- 	    String key = entry.getKey();
-	    if("X-Frame-Options".equals(key)) { 
-              noIFrame = true;
-	    }
+      String linkHref = result.attr("href");
+      String linkText = result.text();
+      if (linkHref.contains("https")) {
+        String url = linkHref.substring(7, linkHref.indexOf("&"));
+        if("/www.google.com/search?num=20".equals(url)) {
+          continue;
+        }
+        URL obj = new URL(url);
+        URLConnection conn = obj.openConnection();
+        Map<String, List<String>> map = conn.getHeaderFields();
+        boolean noIFrame = false;
+        for(Map.Entry<String, List<String>> entry : map.entrySet()) {
+          String key = entry.getKey();
+          if("X-Frame-Options".equals(key)) { 
+            noIFrame = true;
           }
-	  if(!noIFrame) {
-            urls.add(url);
-	  }
-        } 
+        }
+        if(!noIFrame) {
+          urls.add(url);
+        }
+      } 
     }
     return urls;  
   }
